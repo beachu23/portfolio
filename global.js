5f42d5a0ef9d78448d0edf922877463e2f4570a1
beachu23 <brandonchu23@gmail.com>
2025-04-25 20:40:54 -0700

    lab3

--- global.js
+++ global.js
@@ -1,5 +1,70 @@
+// global.js
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'about/', title: 'About' },
+  { url: 'https://github.com/beachu23', title: 'GitHub' }, 
+];
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/"; 
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+  nav.append(a);
+
+  // Highlight the current page
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+  );
+
+  // Open external links in new tab
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+let select = document.querySelector('select');
+
+select.addEventListener('input', function(event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
 }
