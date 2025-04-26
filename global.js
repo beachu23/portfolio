4f24c86feed88d08092de9d77009352cd3dec733
beachu23 <brandonchu23@gmail.com>
2025-04-25 20:54:09 -0700

    dark mode edit

--- global.js
+++ global.js
@@ -9,7 +9,7 @@ let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
-  { url: 'resume/', title: 'Resume' },
+  { url: '../resume/', title: 'Resume' },
   { url: 'https://github.com/beachu23', title: 'GitHub' }, 
 ];
 
